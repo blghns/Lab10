250abd3f62fbda6504a42f971df51204157bbb16
blghns <bilgehan.saglik@gmail.com>
2013-11-12 09:44:12 -0500

    changes

--- Lab10.cpp
+++ Lab10.cpp
@@ -12,97 +12,148 @@ int main ()
 	inStream.open("Twain.txt");
 	outStream.open("output.txt");
 
-	int alphabet[26];
-	for (int i = 0; i < 26; i++)
-	{
-		alphabet[i] = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	int f = 0;
+	int g = 0;
+	int h = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int m = 0;
+	int n = 0;
+	int o = 0;
+	int p = 0;
+	int q = 0;
+	int r = 0;
+	int s = 0;
+	int t = 0;
+	int u = 0;
+	int v = 0;
+	int w = 0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	char charVar;
+	while(inStream >> charVar){
+		if(charVar == 'a' || charVar == 'A')
+			a++;
+		else if(charVar == 'b' || charVar == 'B')
+			b++;
+		else if(charVar == 'c' || charVar == 'C')
+			c++;
+		else if(charVar == 'd' || charVar == 'D')
+			d++;
+		else if(charVar == 'e' || charVar == 'E')
+			e++;
+		else if(charVar == 'f' || charVar == 'F')
+			f++;
+		else if(charVar == 'g' || charVar == 'G')
+			g++;
+		else if(charVar == 'h' || charVar == 'H')
+			h++;
+		else if(charVar == 'i' || charVar == 'I')
+			i++;
+		else if(charVar == 'j' || charVar == 'J')
+			j++;
+		else if(charVar == 'k' || charVar == 'K')
+			k++;
+		else if(charVar == 'l' || charVar == 'L')
+			l++;
+		else if(charVar == 'm' || charVar == 'M')
+			m++;
+		else if(charVar == 'n' || charVar == 'N')
+			n++;
+		else if(charVar == 'o' || charVar == 'O')
+			o++;
+		else if(charVar == 'p' || charVar == 'P')
+			p++;
+		else if(charVar == 'q' || charVar == 'Q')
+			q++;
+		else if(charVar == 'r' || charVar == 'R')
+			r++;
+		else if(charVar == 's' || charVar == 'S')
+			s++;
+		else if(charVar == 't' || charVar == 'T')
+			t++;
+		else if(charVar == 'u' || charVar == 'U')
+			u++;
+		else if(charVar == 'v' || charVar == 'V')
+			v++;
+		else if(charVar == 'w' || charVar == 'W')
+			w++;
+		else if(charVar == 'x' || charVar == 'X')
+			x++;
+		else if(charVar == 'y' || charVar == 'Y')
+			y++;
+		else if(charVar == 'z' || charVar == 'Z')
+			z++;
 	}
 
-	char a;
-	while(inStream >> a){
-		if(a == 'a' || a == 'A')
-			alphabet[0]++;
-		else if(a == 'b' || a == 'B')
-			alphabet[1]++;
-		else if(a == 'c' || a == 'C')
-			alphabet[2]++;
-		else if(a == 'd' || a == 'D')
-			alphabet[3]++;
-		else if(a == 'e' || a == 'E')
-			alphabet[4]++;
-		else if(a == 'f' || a == 'F')
-			alphabet[5]++;
-		else if(a == 'g' || a == 'G')
-			alphabet[6]++;
-		else if(a == 'h' || a == 'H')
-			alphabet[7]++;
-		else if(a == 'i' || a == 'I')
-			alphabet[8]++;
-		else if(a == 'j' || a == 'J')
-			alphabet[9]++;
-		else if(a == 'k' || a == 'K')
-			alphabet[10]++;
-		else if(a == 'l' || a == 'L')
-			alphabet[11]++;
-		else if(a == 'm' || a == 'M')
-			alphabet[12]++;
-		else if(a == 'n' || a == 'N')
-			alphabet[13]++;
-		else if(a == 'o' || a == 'O')
-			alphabet[14]++;
-		else if(a == 'p' || a == 'P')
-			alphabet[15]++;
-		else if(a == 'q' || a == 'Q')
-			alphabet[16]++;
-		else if(a == 'r' || a == 'R')
-			alphabet[17]++;
-		else if(a == 's' || a == 'S')
-			alphabet[18]++;
-		else if(a == 't' || a == 'T')
-			alphabet[19]++;
-		else if(a == 'u' || a == 'U')
-			alphabet[20]++;
-		else if(a == 'v' || a == 'V')
-			alphabet[21]++;
-		else if(a == 'w' || a == 'W')
-			alphabet[22]++;
-		else if(a == 'x' || a == 'X')
-			alphabet[23]++;
-		else if(a == 'y' || a == 'Y')
-			alphabet[24]++;
-		else if(a == 'z' || a == 'Z')
-			alphabet[25]++;
-	}
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	cout << "c: " << c << endl;
+	cout << "d: " << d << endl;
+	cout << "e: " << e << endl;
+	cout << "f: " << f << endl;
+	cout << "g: " << g << endl;
+	cout << "h: " << h << endl;
+	cout << "i: " << i << endl;
+	cout << "j: " << j << endl;
+	cout << "k: " << k << endl;
+	cout << "l: " << l << endl;
+	cout << "m: " << m << endl;
+	cout << "n: " << n << endl;
+	cout << "o: " << o << endl;
+	cout << "p: " << p << endl;
+	cout << "q: " << q << endl;
+	cout << "r: " << r << endl;
+	cout << "s: " << s << endl;
+	cout << "t: " << t << endl;
+	cout << "u: " << u << endl;
+	cout << "v: " << v << endl;
+	cout << "w: " << w << endl;
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+	cout << "z: " << z << endl;
 
-	outStream << "a: " << alphabet[0] << endl;
-	outStream << "b: " << alphabet[1] << endl;
-	outStream << "c: " << alphabet[2] << endl;
-	outStream << "d: " << alphabet[3] << endl;
-	outStream << "e: " << alphabet[4] << endl;
-	outStream << "f: " << alphabet[5] << endl;
-	outStream << "g: " << alphabet[6] << endl;
-	outStream << "h: " << alphabet[7] << endl;
-	outStream << "i: " << alphabet[8] << endl;
-	outStream << "j: " << alphabet[9] << endl;
-	outStream << "k: " << alphabet[10] << endl;
-	outStream << "l: " << alphabet[11] << endl;
-	outStream << "m: " << alphabet[12] << endl;
-	outStream << "n: " << alphabet[13] << endl;
-	outStream << "o: " << alphabet[14] << endl;
-	outStream << "p: " << alphabet[15] << endl;
-	outStream << "q: " << alphabet[16] << endl;
-	outStream << "r: " << alphabet[17] << endl;
-	outStream << "s: " << alphabet[18] << endl;
-	outStream << "t: " << alphabet[19] << endl;
-	outStream << "u: " << alphabet[20] << endl;
-	outStream << "v: " << alphabet[21] << endl;
-	outStream << "w: " << alphabet[22] << endl;
-	outStream << "x: " << alphabet[23] << endl;
-	outStream << "y: " << alphabet[24] << endl;
-	outStream << "z: " << alphabet[25] << endl;
+	outStream << "a: " << a << endl;
+	outStream << "b: " << b << endl;
+	outStream << "c: " << c << endl;
+	outStream << "d: " << d << endl;
+	outStream << "e: " << e << endl;
+	outStream << "f: " << f << endl;
+	outStream << "g: " << g << endl;
+	outStream << "h: " << h << endl;
+	outStream << "i: " << i << endl;
+	outStream << "j: " << j << endl;
+	outStream << "k: " << k << endl;
+	outStream << "l: " << l << endl;
+	outStream << "m: " << m << endl;
+	outStream << "n: " << n << endl;
+	outStream << "o: " << o << endl;
+	outStream << "p: " << p << endl;
+	outStream << "q: " << q << endl;
+	outStream << "r: " << r << endl;
+	outStream << "s: " << s << endl;
+	outStream << "t: " << t << endl;
+	outStream << "u: " << u << endl;
+	outStream << "v: " << v << endl;
+	outStream << "w: " << w << endl;
+	outStream << "x: " << x << endl;
+	outStream << "y: " << y << endl;
+	outStream << "z: " << z << endl;
 
 	inStream.close();
 	outStream.close();
 
+
+	cout << "Written in the output.txt file." << endl;
+	system("PAUSE");
 	return 0;
 }
